f2afd68fe289bccd4421b3ffdb59283706f536cc
rik fabri <rik.fabri@hotmail.be>
2021-06-19 21:00:16 +0200

    Created simple window layout with opengl widget

--- CHIP-8-Emulator_en_BE.ts
+++ CHIP-8-Emulator_en_BE.ts
@@ -4,9 +4,24 @@
 <context>
     <name>Emulator</name>
     <message>
-        <location filename="emulator.ui" line="14"/>
-        <location filename="build/CHIP-8-Emulator_autogen/include_Debug/ui_emulator.h" line="50"/>
+        <location filename="emulator.ui" line="17"/>
+        <location filename="build/CHIP-8-Emulator_autogen/include_Debug/ui_emulator.h" line="72"/>
+        <location filename="build-CHIP-8-Emulator-Desktop_Qt_6_1_0_MinGW_64_bit-Debug/CHIP-8-Emulator_autogen/include/ui_emulator.h" line="76"/>
         <source>Emulator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="emulator.ui" line="55"/>
+        <location filename="build/CHIP-8-Emulator_autogen/include_Debug/ui_emulator.h" line="74"/>
+        <location filename="build-CHIP-8-Emulator-Desktop_Qt_6_1_0_MinGW_64_bit-Debug/CHIP-8-Emulator_autogen/include/ui_emulator.h" line="78"/>
+        <source>ROM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="emulator.ui" line="63"/>
+        <location filename="build/CHIP-8-Emulator_autogen/include_Debug/ui_emulator.h" line="73"/>
+        <location filename="build-CHIP-8-Emulator-Desktop_Qt_6_1_0_MinGW_64_bit-Debug/CHIP-8-Emulator_autogen/include/ui_emulator.h" line="77"/>
+        <source>Load</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
